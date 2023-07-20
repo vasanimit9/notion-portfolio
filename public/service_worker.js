66f73ae89b6c3d76ecff1529424cd834a44279c1
Mit Vasani <vasanimit9@gmail.com>
2023-07-20 18:44:33 +0530

    cache all requests

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,38 +1,42 @@
-let CACHE_NAME = 'amighty_v8_beta_10';
+let CACHE_NAME = "amighty_v8_beta_11";
 
-let URLS_TO_CACHE = [
-  '/',
-  '/blog',
-  '/reading-list'
-];
+let URLS_TO_CACHE = ["/", "/blog", "/reading-list"];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(URLS_TO_CACHE);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (!event.request.url.includes('api') && response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      if (!event.request.url.includes("api") && response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
   );
+  if (event.request.url.includes("api")) {
+    return;
+  }
+
+  caches
+    .open(CACHE_NAME)
+    .then(async (cache) => {
+      await cache.add(event.request.url);
+    })
+    .catch();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   let cacheWhiteList = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheWhiteList.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
